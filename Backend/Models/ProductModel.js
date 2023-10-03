38207373278a9e7ab26fbdde51f63088b5a49837
shiivaniiawasthii <shvaniawsthi001@gmail.com>
2023-10-03 21:14:42 +0530

    updated

--- Backend/Models/ProductModel.js
+++ Backend/Models/ProductModel.js
@@ -1,74 +1,68 @@
 import mongoose from "mongoose";
-const reviewSchema= mongoose.Schema({
-        name:{type:String,require:true},
-        rating: {type:Number,require:true},
-        Comment:{type:String,require:true},
-},{
-        timeStamps:true
-})
-const productSchema= mongoose.Schema({
-        user:{
-                type:mongoose.Schema.Types.ObjectId,
-                required:true,
-                ref:"User"
-        },
-        name:{
-                type:String,
-                required:true
-        },
-        image:{
-                type:String,
-                required:true,
-                
-        },
-        description:{
-                type:String,
-                required:true,
-                
-        },
-        brand:{
-                type:String,
-                required:true,
-                
-        },
-        category:{
-                type:String,
-                required:true,
-                
-        },
-       
-       
-        reviews:[reviewSchema],
-        rating:{
-                type:Number,
-                required:true,
-                default:0
-                
-        },
-        numReviews:{
-                type:Number,
-                required:true,
-                default:0
-                
-        },
-        price:{
-                type:Number,
-                required:true,
-                
-        },
-        countInStock
-        :{
-                type:Number,
-                required:true,
-                default:0
-                
-        }
-},
-        {
-                timeStamps:true
-        
-})
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    rating: { type: Number, require: true },
+    Comment: { type: String, require: true },
+  },
+  {
+    timeStamps: true,
+  }
+);
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
 
-const Product = mongoose.model("Product",productSchema)
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
 
-export default Product
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import {connectDb} from "./Config/db.js"
 import productRoutes from "./Routes/productRouter.js";
 import userRoutes from "./Routes/userRouter.js";
+import path from "path";
 dotenv.config()
 
 connectDb()
@@ -14,12 +15,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 // creating routes
+const PORT = process.env.PORT || 5000
 
 
 
-app.get("/",(req,res)=>{
-        res.send("API is running")
-})
 
 app.use("/api/products",productRoutes);
 app.use("/api/users",userRoutes)
@@ -27,10 +26,19 @@ app.use("/api/users",userRoutes)
 app.get("/api/products",(req,res)=>{
         res.json(products)
 })
+const _dirname = path.resolve()
+if(process.env.NODE_ENV =="production"){
+        app.use(express.static(path.join(_dirname,'/frotend/build')))
+        app.get("*",(req,res)=>res.sendFile(path.resolve(_dirname,'frotend','build','index.html')))
+}else{
+        app.get("/",(req,res)=>{
+                res.send("API is running")
+        })
+}
 
 
 
-const PORT = process.env.PORT || 5000
+
 
 
 

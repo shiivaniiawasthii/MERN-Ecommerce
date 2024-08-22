230846ae34483655f1f5848baaf0c5a4ef5427ea
shiivaniiawasthii <Shvaniawsthi001@gmail.com>
2024-08-22 22:34:21 +0530

    Header,footer and homescreen is pushed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default App
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default App;

f2a1066ea5092ac83c06d40aee2d633421c00c92
shiivaniiawasthii <Shvaniawsthi001@gmail.com>
2024-08-23 12:56:14 +0530

    Hedaer-fixed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,31 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Link to='/'>
+          <Link to="/">
+            {" "}
             <Navbar.Brand>ProShop</Navbar.Brand>
           </Link>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-             <Link to='/cart'>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to="/cart">
+                <Nav.Link>
                   <FaShoppingCart /> Cart
-               </Link>
-              <Link to='/login'>
-               <FaUser /> Sign In
-            </Link>
+                </Nav.Link>
+              </Link>
+              <Link to="/login">
+                {" "}
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>

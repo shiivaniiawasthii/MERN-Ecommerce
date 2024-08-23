5135434b6fc652b9e4f6c211749a0e1628bf6264
shiivaniiawasthii <Shvaniawsthi001@gmail.com>
2024-08-23 11:42:45 +0530

    React-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main className="py-3">
+      <main className="py-4">
         <Container>
-          <HomeScreen />
+          <h1>Welcome to Photoshop</h1>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
+
 export default App;

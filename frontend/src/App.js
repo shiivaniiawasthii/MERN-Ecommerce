7d72090505a317fc357342bf3a77f20c53078358
shiivaniiawasthii <Shvaniawsthi001@gmail.com>
2024-08-23 12:49:23 +0530

    Product screen and customize router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Outlet} from "react-router-dom";
+
 
 const App = () => {
   return (
-    <Router>
+   <>
       <Header />
       <main className="py-4">
         <Container>
-          <h1>Welcome to Photoshop</h1>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Outlet />
         </Container>
       </main>
       <Footer />
-    </Router>
+</>
+ 
   );
 };
 
